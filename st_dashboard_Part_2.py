4e4efa818b7b83c509d40fff1693a8fa928b85dc
Faisal Malekzada <faisalmalikzada1@gmail.com>
2025-10-29 11:10:12 +0100

    Update st_dashboard_Part_2.py

--- st_dashboard_Part_2.py
+++ st_dashboard_Part_2.py
@@ -4,8 +4,6 @@ import numpy as np
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
-from streamlit_keplergl import keplergl_static
-from keplergl import KeplerGl
 from datetime import datetime as dt
 from numerize.numerize import numerize
 from PIL import Image
